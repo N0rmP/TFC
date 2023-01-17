39f5f69cf2194a254b3d87214a790a4d1ec09231
N0rmP <67183361+N0rmP@users.noreply.github.com>
2023-01-17 12:59:54 +0900

    creature.take_damage 개선
    매개변수로 어떤 액터로부터 피해를 받았는지, 어느 부위에 피격되었는지 추가

--- TFC_Project/Intermediate/Build/Win64/TFC_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ TFC_Project/Intermediate/Build/Win64/TFC_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,1 +1,1 @@
-#include "C:/Unreal Projects/TFC/TFC/TFC_Project/Intermediate/Build/Win64/TFC_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "C:/Users/G418-24/Desktop/TFC_temp/TFC/TFC_Project/Intermediate/Build/Win64/TFC_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
