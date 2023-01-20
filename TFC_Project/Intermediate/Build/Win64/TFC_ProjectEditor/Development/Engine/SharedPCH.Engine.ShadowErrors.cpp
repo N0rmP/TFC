d2b49ad86d8985922092e8bf800498be010fee52
UmGnJin <86461489+UmGnJin@users.noreply.github.com>
2023-01-20 20:47:13 +0900

    230120 oncollection
    oncollection 생성 및 모듈, 업그레이드, 이펙트 하위로 정리 완료. oncollection이 actor component 상속함에 따라 일부 BP 변경처리.
    * 4.26.2 이상부터 add component by class가 추가됨에 따라 해당 기능 이용을 위해 상위버전에서 프로젝트 실행 테스트 예정.
    
    creature가 effect 추가/제거하는 함수 추가. 에러 제거를 위해 effect의 생성/삭제 사이클 간 예외처리 추가.

--- TFC_Project/Intermediate/Build/Win64/TFC_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ TFC_Project/Intermediate/Build/Win64/TFC_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/G418-24/Desktop/TFC_start/TFC/TFC_Project/Intermediate/Build/Win64/TFC_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "C:/Unreal Projects/TFC/TFC/TFC_Project/Intermediate/Build/Win64/TFC_ProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
